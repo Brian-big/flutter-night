a3a6f0504aebcfa20605d3087c7d628bebd1e734
Brian Barasa <briannbig@gmail.com>
2023-03-19 07:06:21 +0300

    add favourites logic

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,17 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
+  var favorites = <WordPair>[];
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
+
   void getNext() {
     current = WordPair.random();
     notifyListeners();
